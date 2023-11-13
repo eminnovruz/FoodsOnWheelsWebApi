c1ce2ebfb0f198f8e111f38ae862b7a7baebd39f
Novr_wz05 <Novr_wz05@STHQ0118-04>
2023-11-13 21:04:21 +0400

    Folder names changed, .ignore file edited

---
+++ Core/Application/Models/DTOs/User/GetUserProfileInfoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Models.DTOs.User;
+
+public class GetUserProfileInfoDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }   
+    public string PhoneNumber { get; set; }
+    public DateTime BirthDate { get; set; }
+    public List<string> OrderIds { get; set; }
+}
