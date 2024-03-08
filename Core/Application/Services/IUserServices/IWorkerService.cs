16b3fc5c179dc78bec06108b518df3a165c6a516
Ibra_xj86 <Ibra_xj86@STHQ0118-02>
2024-03-08 19:34:39 +0400

    IWorker updated

--- Core/Application/Services/IUserServices/IWorkerService.cs
+++ Core/Application/Services/IUserServices/IWorkerService.cs
@@ -10,14 +10,19 @@ namespace Application.Services.IUserServices;
 public interface IWorkerService
 {
     Task<bool> AddRestaurant(AddRestaurantDto dto);
-    Task<bool> AddCourier(AddCourierDto dto);
-    Task<IEnumerable<RestaurantInfoDto>> GetAllRestaurants();
+    Task<bool> UptadeRestaurant(UpdateRestaurantDto dto);
     Task<bool> RemoveRestaurant(string restaurantId);
+    Task<IEnumerable<RestaurantInfoDto>> GetAllRestxaurants();
+    Task<bool> AddCourier(AddCourierDto dto);
+    Task<bool> UpdateCourier(UpdateCourierDto dto);
     Task<bool> RemoveCourier(string courierId);
-    Task<IEnumerable<Food>> SeeAllFoods();
     Task<IEnumerable<SummaryCourierDto>> GetAllCouriers();
-    Task<bool> AddCategory(AddCategoryRequest request);
-    Task<bool> RemoveCategory(string Id);
     bool AddNewFood(AddFoodRequest request);
+    bool UpdateFood(UpdateFoodRequest request);
     Task<bool> RemoveFood(string Id);
+    Task<IEnumerable<Food>> SeeAllFoods();
+    Task<bool> AddCategory(AddCategoryRequest request);
+    Task<bool> UpdateCategory(UpdateCategoryRequest request);
+    Task<bool> RemoveCategory(string Id);
+    Task<IEnumerable<Category>> SeeAllCategories();
 }
